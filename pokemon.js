3768c8a5ec10e8d7978d9206d96ddd740217409a
Dennis Budde <dennis@writerduet.com>
2023-06-27 19:48:14 -0500

    added error handling and handled submit better to include enter key

--- pokemon.js
+++ pokemon.js
@@ -1,23 +1,44 @@
 let button = document.querySelector("#searchButton")
 
-button.addEventListener('click', async () => {
+let pokemonName = document.querySelector("#pokemonName")
+let pokemonImage = document.querySelector("#pokemonImage")
+let pokemonHp = document.querySelector("#pokemonHp")
+let pokemonType = document.querySelector("#pokemonType")
+let pokemonAbility = document.querySelector(".pokemonAbility")
 
-    let textInput = document.querySelector("#inputBar").value
-    let pokemonName = document.querySelector("#pokemonName")
-    let pokemonImage = document.querySelector("#pokemonImage")
-    let pokemonHp = document.querySelector("#pokemonHp")
-    let pokemonType = document.querySelector("#pokemonType")
-    let pokemonAbility = document.querySelector(".pokemonAbility")
+let pokeInput = document.querySelector(".pokeInput")
+let inputBar = document.querySelector("#inputBar");
+let infoText = document.querySelector("#infoText");
 
+window.addEventListener('load', async ()=> {
+    let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+    setPokeCard(response.data);
+})
+
+pokeInput.addEventListener('submit', (event) => {
+        pokeSearch();
+        event.preventDefault();
+})
+
+async function pokeSearch() {
+    let textInput = inputBar.value;
 
     //Axios call goes here
     //remember to use Await!
-    let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${textInput}`);
+    let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${textInput}`)
+        .catch(error => {
+            infoText.innerText = "We couldn't find that Pokemon. Double-check your spelling and try again!";
+        })
     //DOM Setters go here
-    pokemonName.innerText = response.data.name + '!';
-    pokemonImage.innerHTML = `<img src=${response.data.sprites.other['official-artwork'].front_default} />`;
-    pokemonHp.innerText = "HP: " + response.data.stats[0].base_stat;
-    pokemonType.innerText = "Type: " + response.data.types[0].type.name;
-    pokemonAbility.innerText = "Primary Ability: " + response.data.abilities[0].ability.name;
-})
+    setPokeCard(response.data);
+    inputBar.value = "";
+    infoText.innerText = "";
+}
 
+function setPokeCard(data) {
+        pokemonName.innerText = data.name + '!';
+        pokemonImage.innerHTML = `<img src=${data.sprites.other['official-artwork'].front_default} />`;
+        pokemonHp.innerText = "HP: " + data.stats[0].base_stat;
+        pokemonType.innerText = "Type: " + data.types[0].type.name;
+        pokemonAbility.innerText = "Primary Ability: " + data.abilities[0].ability.name;
+}
